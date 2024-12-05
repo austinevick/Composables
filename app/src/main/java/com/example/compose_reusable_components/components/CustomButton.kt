7a5decfcd5c3308452dae4dd7a4b3db60a40f774
Augustine Victor <augustinevickky@gmail.com>
2024-12-05 12:40:39 +0100

    Update CustomButton.kt

--- app/src/main/java/com/example/compose_reusable_components/components/CustomButton.kt
+++ app/src/main/java/com/example/compose_reusable_components/components/CustomButton.kt
@@ -1,31 +1,59 @@
 package com.example.compose_reusable_components.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CustomButton(
     isLoading: Boolean = false,
-    text: String = "",
+    enabled: Boolean = true,
+    color: Color = Color.White,
+    height: Dp = 50.dp,
+    width: Dp? = null,
+    radius: Dp = 8.dp,
+    text: String? = null,
+    textColor: Color = Color.White,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    borderColor: Color = Color.Transparent,
+    borderWidth: Dp = 1.dp,
+    content: @Composable () -> Unit = {},
     onClick: () -> Unit
 ) {
     val localConfig = LocalConfiguration.current
 
     Button(
+        enabled = enabled,
         onClick = onClick,
-        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(borderWidth, borderColor),
+        colors = ButtonDefaults.buttonColors(containerColor = color),
+        shape = RoundedCornerShape(radius),
         modifier = Modifier
-            .width(localConfig.screenWidthDp.dp)
-            .height(55.dp)
+            .width(width ?: localConfig.screenWidthDp.dp)
+            .height(height)
     ) {
-        if (isLoading) CircularProgressIndicator() else Text(text = text)
+        if (isLoading) CircularProgressIndicator(
+            modifier = Modifier.size(24.dp),
+            strokeWidth = 2.dp,
+            color = Color.White
+        )
+        else if (text != null) Text(
+            text = text,
+            fontSize = fontSize,
+            color = textColor
+        ) else content()
     }
 }
